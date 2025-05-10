d56d1fdf39709be752f819d20d5a21a437071b92
Helena Kjeld <118178627+HelenaKjeld@users.noreply.github.com>
2025-05-10 23:52:54 +0200

    hero_section

--- app/public/wp-content/themes/Test Blog/index.php
+++ app/public/wp-content/themes/Test Blog/index.php
@@ -1,7 +1,27 @@
 <?php get_header(); ?>
 
+
+
+
 <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
+
+
+
+    <?php 
+        $heroImage = get_field("heroimage");
+        $herotitle = get_field("herotitle");
+    ?>
+
+    <header class="hero_section_container">
+        <div class="hero_img_">
+        <img src="<?php echo $heroImage['url']; ?>" class="hero_img">
+        </div> 
+        <div class="hero_title">
+                <h1><?php echo $herotitle;?></h1>
+        </div>
+          
+    </header>
 
 
     <?php endwhile; ?>
